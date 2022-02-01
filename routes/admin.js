6601f4352eff93f85390d61974ad9a2f888c7e59
vignesh kundar <vigneshkundar125@gmail.com>
2022-02-01 12:57:09 +0530

    Add session

--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,29 @@
 const express = require('express')
 const sqlite = require('sqlite3').verbose();
+const sessions = require('express-session');
 
+const db = new sqlite.Database('./SHOP-DB')
 
-
-const router = express.Router()
+const router = express.Router();
+const oneDay = 1000 * 60 * 60 * 24; //cookie for one day session
+var session
+    //session middleware
+router.use(sessions({
+    secret: "MulleMadarChodBc",
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}));
 
 
 router.get('/', (req, res) => {
-    res.render("admin");
-
-})
-
-router.post("/", (req, res) => {
 
     const db = new sqlite.Database('./SHOP-DB')
 
-    shops = []
-    products = []
-    orders = []
+    session = req.session;
 
-    if (req.body.usrname != "admin")
-        res.send('<script> alert("Invalid Username !!!") </script>');
-    else if (req.body.password != "123")
-        res.send('<script> alert("Invalid Password !!!") </script>');
-    else {
+    if (session.userid) {
+
 
         db.serialize(() => {
 
@@ -71,6 +71,9 @@ router.post("/", (req, res) => {
 
         }); //end of serialize :)
 
+
+
+
         db.close((err) => {
             err ? console.log(err) : res.render("dashboard", { Angadi: shops, Saman: products, Orders: orders });
         });
@@ -80,8 +83,93 @@ router.post("/", (req, res) => {
 
 
 
+    } else {
+        res.render("admin");
+    }
+
+})
+
+router.post("/", (req, res) => {
+
+    const db = new sqlite.Database('./SHOP-DB')
+
+    shops = []
+    products = []
+    orders = []
+
+    if (req.body.usrname == "admin" && req.body.password == "123") {
+        session = req.session;
+        session.userid = req.body.usrname;
+        console.log("==Session==" + req.sesssion);
+
+        db.serialize(() => {
+
+            db.all(`SELECT * FROM SHOP`, (err, row) => {
+
+                if (err)
+                    console.log(err)
+                else {
+                    let i = 0;
+
+                    row.forEach((shop) => {
+                        shops[i++] = shop;
+                    })
+                }
+            })
+
+
+            db.all(`SELECT * FROM PRODUCTS`, (err, row) => {
+
+                if (err)
+                    console.log(err)
+                else {
+                    let j = 0;
+
+                    row.forEach((rev) => {
+                        products[j++] = rev;
+                    })
+                }
+                console.log(products);
+            })
+
+            db.all(`SELECT * FROM ORDERS`, (err, row) => {
+
+                if (err)
+                    console.log(err)
+                else {
+                    let j = 0;
+
+                    row.forEach((rev) => {
+                        orders[j++] = rev;
+                    })
+                }
+                console.log(products);
+            })
+
+        }); //end of serialize :)
+
+
+
+
+        db.close((err) => {
+            err ? console.log(err) : res.render("dashboard", { Angadi: shops, Saman: products, Orders: orders });
+        });
 
     }
+
+    // if (req.body.usrname != "admin")
+    //     res.send('<script> alert("Invalid Username !!!") </script>');
+    // else if (req.body.password != "123")
+    //     res.send('<script> alert("Invalid Password !!!") </script>');
+    // else {
+
+    // }
+
+})
+
+router.get("/logout", (req, res) => {
+    req.session.destroy();
+    res.redirect("/admin")
 })
 
 
